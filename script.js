6fe57a5bf0061eb6a417bbb989ad44d0a6fbd912
AmI2077 <164494309+AmI2077@users.noreply.github.com>
2024-12-08 20:12:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let isClicked = false;
 
 button.addEventListener('click', function ()
 {
+    tg.MainButton.show()
     if (!isClicked) {
         button.textContent = 'Добавлено';
         button.style.backgroundColor = '#14d82e';
