f1dfd7e81679057f401b7559d2bac64f43777ea4
AmI2077 <164494309+AmI2077@users.noreply.github.com>
2024-12-08 20:24:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let tg = window.Telegram.WebApp; 
-tg.expand()
+tg.expand(); 
+tg.MainButton.textContent("Главная кнопка");
 
-var button = document.getElementById('button_1')
+var button = document.getElementById('button_1');
 let isClicked = false;
 
 button.addEventListener('click', function ()
 {
-    tg.MainButton.show()
+
     if (!isClicked) {
         button.textContent = 'Добавлено';
         button.style.backgroundColor = '#14d82e';
