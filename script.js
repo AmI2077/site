cd2b453367f5a6251035191aeb3bc6fa0e6d3b89
AmI2077 <164494309+AmI2077@users.noreply.github.com>
2024-12-08 19:30:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+let tg = window.Telegram.WebApp; 
+
+
 var button = document.getElementById('button_1')
+let isClicked = false;
 
 button.addEventListener('click', function ()
 {
-    alert('Товар был добавлен в корзину');
+    if (!isClicked) {
+        button.textContent = 'Добавлено';
+        button.style.backgroundColor = '#14d82e';
+    }
+    else
+    {
+        button.textContent = '+ Добавить';
+        button.style.backgroundColor = '#eea60a';
+    }    
+    isClicked = !isClicked;
+    tg.sendData('Товар добавлен'); 
 }
 )
