90cc5f994c5686c86ff437bbb097a89d36faf3b2
AmI2077 <164494309+AmI2077@users.noreply.github.com>
2024-12-08 20:35:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let tg = window.Telegram.WebApp; 
 tg.expand(); 
 
-tg.MainButton.textColor = "#FFFFFF"
-tg.MainButton.color = "#2cab37"
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
 
-var button = document.getElementById('button_1');
+var button = document.getElementById('button_1'); 
 let isClicked = false;
 
-button.addEventListener('click', function ()
-{
+button.addEventListener('click', function () {
 
     if (!isClicked) {
         button.textContent = 'Добавлено';
         button.style.backgroundColor = '#14d82e';
     }
-    else
-    {
+    else {
         button.textContent = '+ Добавить';
         button.style.backgroundColor = '#eea60a';
-    }    
+    }
+    
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Оформить");
+        tg.MainButton.show();
+    }
     isClicked = !isClicked;
-    tg.sendData('Товар добавлен'); 
-}
-)
+    tg.sendData('Товар добавлен');
+}); 
