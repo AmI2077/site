e895b2b7a389c6d98c832c578e879eab1599426a
AmI2077 <164494309+AmI2077@users.noreply.github.com>
2024-12-15 00:29:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,27 +6,32 @@ tg.MainButton.color = "#FAC000";
 
 var buttons = document.querySelectorAll('.button'); 
 var prices = document.querySelectorAll('.price').textContent;
+var items = document.querySelectorAll('.title').textContent;
 
 let totalprice = [];
+let order = new Map();
 var totalSum = 0; 
 
 buttons.forEach(button => {
     let isClicked = false;
     button.addEventListener('click', function () {
         const item = button.closest('.item');
+        const title = item.querySelector('.title').textContent;
         const price = item.querySelector('.price').textContent;
         let int_price = parseInt(price);
 
         if (!isClicked) {
             totalprice.push(int_price);
+            order.set(title, int_price)
             button.textContent = 'Добавлено';
             button.style.backgroundColor = '#14d82e';
             totalSum = totalprice.reduce((a, b) => a + b, 0);
-            alert(`Цена товара: ${totalSum}`);
+            //alert(`Цена товара: ${totalSum}`);
             tg.MainButton.setText(`Общая стоимость: ${totalSum}`);
             tg.MainButton.show();
         }
         else {
+            order.delete(title);
             const index = totalprice.indexOf(int_price);
             if (index > -1) {
                 totalprice.splice(index, 1); 
@@ -43,5 +48,5 @@ buttons.forEach(button => {
 
 Telegram.WebApp.onEvent('mainButtonClicked', function () {
 
-    tg.sendData(`пожалуйста ${JSON.stringify(totalprice)}`);
+    tg.sendData(`Заказ ${JSON.stringify(order)}`);
 }); 
